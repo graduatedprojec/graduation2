18338cc707a9f02b37e819240409e1c4abb71a0f
Moaz Karem <Moazkarem@gmail.com>
2024-05-03 01:15:12 +0300

    finish add lab

--- src/pages/Labs/Add Lab/AddLab.jsx
+++ src/pages/Labs/Add Lab/AddLab.jsx
@@ -3,29 +3,71 @@ import Input from "../../../Ui/Input";
 import Label from "../../../Ui/Label";
 import SubmitButton from "../../../components/Submit Button/SubmitButton";
 import Cancelbtn from "../../../components/Cancel Button/Cancelbtn";
-
+import toast from "react-hot-toast";
+import { useState } from "react";
+import UseAddLab from "../../../hooks/HLabs/UseAddLab";
 // eslint-disable-next-line react/prop-types
 const AddLab = ({ isOpen, closeModal, title }) => {
+  const [labData, setlabData] = useState({
+    name: "",
+
+  });
+  const { addLabData, loading } = UseAddLab();
+  const storageKey = "logged";
+  const userDataString = localStorage.getItem(storageKey);
+  const userData = userDataString ? JSON.parse(userDataString) : null;
+  const changeHandler = (e) => {
+    const { value, name } = e.target;
+    setlabData({
+      ...labData,
+      [name]: value,
+    });
+  };
+  const onAddHandler = async (e) => {
+    e.preventDefault();
+    const result = await addLabData(labData, userData);
+    if (result.success) {
+      closeModal();
+      toast.success("Success Adding!", {
+        duration: 1000,
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+      setTimeout(() => {
+        window.location.reload();
+      }, 1500);
+    } else {
+      toast.error(result.error, {
+        duration: 1000,
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+    }
+  };
   return (
     <div>
       <Modal title={title} isOpen={isOpen} closeModal={closeModal}>
-        <form>
-          <div className="flex gap-2 flex-col">
-            <Label htmlFor="id"> ID : </Label>
-            <Input 
-// @ts-ignore
-            id="id" />
-          </div>
-          <div className="flex gap-2 flex-col">
-            <Label htmlFor="Name">Name  : </Label>
-            <Input 
-// @ts-ignore
-            id="Name" />
+        <form onSubmit={onAddHandler}>
+        <div className="flex gap-2 flex-col">
+            <Label htmlFor="Name"> Name : </Label>
+            <Input
+              // @ts-ignore
+              id="Name"
+              value={labData.name}
+              onChange={changeHandler}
+              name="name"
+            />
           </div>
       
-          <div className="flex justify-center items-center space-x-3 ">
-            <SubmitButton title={"Submit"} add={""} />
-            <Cancelbtn title={"Cancel"} add={""} />
+          <div className="flex justify-center items-center space-x-3">
+            <SubmitButton loading={loading}> Add </SubmitButton>
+            <Cancelbtn onClick={closeModal}>Cancel</Cancelbtn>
           </div>
         </form>
       </Modal>
